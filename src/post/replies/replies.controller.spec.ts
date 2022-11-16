87139b8db0a2fa9d8dd8565621586242a13ec1fd
Ben Miller <bjmiller86@gmail.com>
2022-11-15 19:48:50 -0800

    Added replies controller test.

--- src/post/replies/replies.controller.spec.ts
+++ src/post/replies/replies.controller.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PostModule } from '../post.module';
 import { RepliesController } from './replies.controller';
+import { RepliesService } from './replies.service';
 
 describe('RepliesController', () => {
   let controller: RepliesController;
 
-  beforeEach(async () => {
+  beforeEach(async () => 
+  {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RepliesController],
+      providers: [RepliesService],
+      imports: [PostModule]
+
     }).compile();
 
-    controller = module.get<RepliesController>(RepliesController);
+    controller = await module.resolve(RepliesController);
   });
 
-  it('should be defined', () => {
+  it('should be defined', () => 
+  {
     expect(controller).toBeDefined();
   });
 });
